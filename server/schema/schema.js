9240448e94437828842298e0cbacd3232983aa35
Druidan <44009123+Druidan@users.noreply.github.com>
2020-08-14 15:53:37 -0500

    I tweaked some things to make the relational data work when looking for all comments associated with a particular article.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,21 +3,23 @@ const _ = require('lodash')
 
 const { 
     GraphQLID,
+    GraphQLList,
     GraphQLObjectType, 
     GraphQLSchema, 
-    GraphQLString } = graphql
+    GraphQLString 
+} = graphql
 
 //dummy data
 const articles = [
-    {title: 'Why Halo is Good', genre: 'Opinion', id:'1'},
-    {title: 'Why Halo is Bad and Why My Coworker is an Idiot With Bad Tastes', genre: 'Opinion', id:'2'},
-    {title: 'Gamers Kinda Suck Sometimes', genre: 'FACTS', id:'3'},
+    {title: 'Why Halo is Good', genre: 'Opinion', id:'1', commentid: '1'},
+    {title: 'Why Halo is Bad and Why My Coworker is an Idiot With Bad Tastes', genre: 'Opinion', id:'2', commentid: '3'},
+    {title: 'Gamers Kinda Suck Sometimes', genre: 'FACTS', id:'3', commentid: '4'},
 ]
 const comments = [
-    {title: 'Comment 1', content: 'This guy gets it.', id:'1'},
-    {title: 'Comment 2', content: 'Obvs bought off by Microsuuuck.', id:'2'},
-    {title: 'Comment 3', content: 'Wow, so salty, tho.', id:'3'},
-    {title: 'Comment 4', content: 'FACTS!', id:'4'},
+    {title: 'Comment 1', content: 'This guy gets it.', id:'1', articleId: '1'},
+    {title: 'Comment 2', content: 'Obvs bought off by Microsuuuck.', id:'2', articleId: '1'},
+    {title: 'Comment 3', content: 'Wow, so salty, tho.', id:'3', articleId: '2'},
+    {title: 'Comment 4', content: 'FACTS!', id:'4', articleId: '3'},
 ]
 
 const ArticleType = new GraphQLObjectType({
@@ -26,6 +28,12 @@ const ArticleType = new GraphQLObjectType({
         id: { type: graphql.GraphQLID },
         title: { type: GraphQLString },
         genre: { type: GraphQLString },
+        comments: {
+            type: new GraphQLList(CommentType),
+            resolve(parent, args){
+                return _.filter(comments, { articleId: parent.id})
+            }
+        }
     })
 })
 
@@ -35,6 +43,12 @@ const CommentType = new GraphQLObjectType({
         id: { type: GraphQLID },
         title: { type: GraphQLString },
         content: { type: GraphQLString },
+        article: {
+            type: ArticleType,
+            resolve(parent, args){
+                return _.find(articles, { id: parent.articleId})
+            }
+        }
     })
 })
 
