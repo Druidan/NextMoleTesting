bf0e9122198fef4261e675c29166184325ca6f13
Druidan <44009123+Druidan@users.noreply.github.com>
2020-08-22 18:13:52 -0500

    I updated the resolver functions to use the Mongo DB.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,21 +12,6 @@ const {
     GraphQLString 
 } = graphql
 
-//mongo "mongodb+srv://cluster0-q3cpb.mongodb.net/<dbname>" --username molemaster
-
-//dummy data
-// const articles = [
-//     {title: 'Why Halo is Good', genre: 'Opinion', id:'1', commentIds: ['1', '2']},
-//     {title: 'Why Halo is Bad and Why My Coworker is an Idiot With Bad Tastes', genre: 'Opinion', id:'2', commentIds: ['3']},
-//     {title: 'Gamers Kinda Suck Sometimes', genre: 'FACTS', id:'3', commentIds: ['4']},
-// ]
-// const comments = [
-//     {title: 'Comment 1', content: 'This guy gets it.', id:'1', articleId: '1'},
-//     {title: 'Comment 2', content: 'Obvs bought off by Microsuuuck.', id:'2', articleId: '1'},
-//     {title: 'Comment 3', content: 'Wow, so salty, tho.', id:'3', articleId: '2'},
-//     {title: 'Comment 4', content: 'FACTS!', id:'4', articleId: '3'},
-// ]
-
 const ArticleType = new GraphQLObjectType({
     name: 'Article',
     fields: () => ({
@@ -39,7 +24,7 @@ const ArticleType = new GraphQLObjectType({
         comments: {
             type: new GraphQLList(CommentType),
             resolve(parent, args){
-                // return _.filter(comments, { articleId: parent.id})
+                return Comment.find({articleId: parent.id})
             }
         }
     })
@@ -54,7 +39,7 @@ const CommentType = new GraphQLObjectType({
         article: {
             type: ArticleType,
             resolve(parent, args){
-                // return _.find(articles, { id: parent.articleId})
+                return Article.findById(parent.authorId)
             }
         }
     })
@@ -67,28 +52,30 @@ const RootQuery = new GraphQLObjectType({
             type: ArticleType,
             args: {id: { type: GraphQLID }},
             resolve(parent, args){
-                //code to grb data from db
                 // return _.find(articles, { id: args.id })
+                return Article.findById(args.id)
             }
         },
         comment: {
             type:CommentType,
             args: {id: { type: GraphQLID }},
             resolve(parent, args){
-                //code to grb data from db
                 // return _.find(comments, { id: args.id })
+                return Comment.findById(args.id)
             }
         },
         articles: {
             type: new GraphQLList(ArticleType),
             resolve(parent, arg){
                 // return articles
+                return Article.find({})
             }
         },
         comments: {
             type: new GraphQLList(CommentType),
             resolve(parent, args) {
                 // return comments
+                return Comment.find({})
             }
         }
     }
@@ -119,6 +106,22 @@ const Mutation = new GraphQLObjectType({
                 return article.save()
             }
         },
+        addComment: {
+            type: CommentType,
+            args: {
+                commentor: {type: GraphQLString},
+                content: {type: GraphQLString},
+                articleId: {type: GraphQLID}
+            },
+            resolve(parent, args) {
+                let comment = new Comment({
+                    commentor: args.commentor,
+                    content: args.content,
+                    articleId: args.articleId
+                })
+                return comment.save()
+            }
+        }
     }
 })
 
